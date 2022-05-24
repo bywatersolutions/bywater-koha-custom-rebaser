fca094f600a9101113fd426bb0a113abba01cd67
Kyle M Hall <kyle@bywatersolutions.com>
2022-05-24 13:16:10 -0400

    Enable safe.directory

--- bws_rebase_branches.pl
+++ bws_rebase_branches.pl
@@ -35,6 +35,8 @@ if ( $ENV{KOHA_BRANCH} ) {
 
 my $ua = LWP::UserAgent->new;
 
+say "Setting current dir as safe directory";
+qx{ git config --global --add safe.directory . };
 say "Removing existing github repo, if any";
 qx{ git remote rm github };
 say "Adding github repo";
